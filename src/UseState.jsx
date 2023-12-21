e91bf9c67bccd58ddd07e713ae7529609b95403c
Brad <bradcochi@gmail.com>
2023-12-20 23:25:38 -0700

    Input state displaying correctly

--- src/UseState.jsx
+++ src/UseState.jsx
@@ -1,7 +1,33 @@
 import { useState } from "react";
 
-const UseState = () => {
-  return "Hello World!";
-};
+function UseState() {
+  const [inputName, setInputName] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Name..."
+        onChange={(e) => {
+          setInputName(e.target.value);
+        }}
+      />
+      <h1>{inputName}</h1>
+    </div>
+  );
+}
 
 export default UseState;
+
+// const [count, setCount] = useState(0);
+// return (
+//   <>
+//     <div>{count}</div>
+//     <button
+//       onClick={() => {
+//         setCount(count + 1);
+//       }}
+//     >
+//       Increase Count
+//     </button>
+//   </>
+// );
