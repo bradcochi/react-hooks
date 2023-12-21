fcae99b8ed887cca115410a607b19eac38b5a5f5
Brad <bradcochi@gmail.com>
2023-12-20 23:44:47 -0700

    added buttons to UseEffect page

--- src/UseEffect.jsx
+++ src/UseEffect.jsx
@@ -1,5 +1,15 @@
 import { useState, useEffect } from "react";
 
-function UseEffect() {}
+function UseEffect() {
+  const [count, setCount] = useState();
+
+  return (
+    <div>
+      <h3>Count: {count}</h3>
+      <button>Decrement</button>
+      <button>Increment</button>
+    </div>
+  );
+}
 
 export default UseEffect;
