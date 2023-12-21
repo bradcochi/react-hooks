1b1570849f9a1d1d17daf2b33b9e5aaf09351d71
Brad <bradcochi@gmail.com>
2023-12-20 23:47:18 -0700

    added decrement/increment functionality

--- src/UseEffect.jsx
+++ src/UseEffect.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 
 function UseEffect() {
-  const [count, setCount] = useState();
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <h3>Count: {count}</h3>
-      <button>Decrement</button>
-      <button>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
   );
 }
