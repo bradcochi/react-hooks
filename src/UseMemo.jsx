bc17ca42c6c5d8092156ae8c370ef2c84508a88c
Brad <bradcochi@gmail.com>
2024-02-02 22:28:46 -0700

    fixed errors with selectedItem returning undefined

--- src/UseMemo.jsx
+++ src/UseMemo.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { initialItems } from "./utils";
 
-interface DemoProps {}
+let DemoProps = {};
 
-function useMemoDemo() {
+function useMemoDemo(DemoProps) {
   const [count, setCount] = useState(0);
   const [items] = useState(initialItems);
 
@@ -12,7 +12,7 @@ function useMemoDemo() {
   return (
     <div>
       <h1>Count: {count}</h1>
-      <h1>Selected Items: {selectedItem?.id}</h1>
+      <h1>Selected Item: {selectedItem?.id}</h1>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
