2d1250d5769fb1710e2e47a9a285cf1941bfb80b
Brad <bradcochi@gmail.com>
2024-02-02 22:49:01 -0700

    created UseReducer file and added its path to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import UseState from "./UseState";
 import UseEffect from "./UseEffect";
 import UseMemo from "./UseMemo";
+import UseReducer from "./UseReducer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/usestate" element={<UseState />} />
           <Route path="/useeffect" element={<UseEffect />} />
           <Route path="/usememo" element={<UseMemo />} />
+          <Route path="/usereducer" element={<UseReducer />} />
         </Routes>
       </main>
     </div>
