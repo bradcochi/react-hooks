98331aec72dd73fdb78a55833ed9a126930c79f4
Brad <bradcochi@gmail.com>
2024-02-02 22:54:51 -0700

    imported useReducer and began defining State and Action variables

--- src/UseReducer.jsx
+++ src/UseReducer.jsx
@@ -1,3 +1,12 @@
+import { useReducer } from "react";
+
+// let State = {
+//   count: number,
+//   error: string | null,
+// };
+
 export default function UseReducerDemo() {
+  const [state, dispatch] = useReducer();
+
   return <div className="tutorial"></div>;
 }
